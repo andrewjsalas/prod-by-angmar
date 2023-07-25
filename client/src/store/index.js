c7e579c71c4d76848349ed88bbf2a8fae1b9a6bf
asalas <67822688+andrewjsalas@users.noreply.github.com>
2023-07-24 18:55:02 -0700

    Add more template stuff

---
+++ client/src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore, createSlice } from '@redux/toolkit';
+
+// Create slice for auth state
+
+// Export auth actions
+
+// Configure the Redux store with the auth slice as the reducer
+
